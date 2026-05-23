775813df57cd34c17374456173a35f201139fa56
Abraham Vivas <vivasabrahan02@gmail.com>
2026-05-22 20:30:34 -0400

    ADD: .bat inicializador ms

---
+++ iniciar-todo.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Iniciando todos los microservicios...
+
+start cmd /k "cd /d %~dp0ms-recintos\recintos && mvnw.cmd spring-boot:run"
+start cmd /k "cd /d %~dp0ms-eventos\eventos && mvnw.cmd spring-boot:run"
+start cmd /k "cd /d %~dp0ms-tickets\tickets && mvnw.cmd spring-boot:run"
+start cmd /k "cd /d %~dp0ms-ventas\ventas && mvnw.cmd spring-boot:run"
+start cmd /k "cd /d %~dp0ms-validacion\validacion && mvnw.cmd spring-boot:run"
+start cmd /k "cd /d %~dp0ms-artistas\artistas && mvnw.cmd spring-boot:run"
+start cmd /k "cd /d %~dp0ms-preventa\preventa && mvnw.cmd spring-boot:run"
+start cmd /k "cd /d %~dp0ms-devoluciones\devoluciones && mvnw.cmd spring-boot:run"
+start cmd /k "cd /d %~dp0ms-promotores\promotores && mvnw.cmd spring-boot:run"
+start cmd /k "cd /d %~dp0ms-streaming\streaming && mvnw.cmd spring-boot:run"
+
+echo Todos los microservicios iniciando...
